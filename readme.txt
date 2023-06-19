f51f7b744985e369ada8dae8162c12d8906adbe5
Justin Brierley <91489058+paradingwolves@users.noreply.github.com>
2023-06-19 13:16:22 -0400

    Initial Commit

---
+++ readme.txt
@@ -0,0 +1,16 @@
+Date Created: June 19, 2023
+Project Name: Nut Face
+Project Purpose: This is a social media outlet for memes
+Project Author: Justin Brierley
+
+
+Date: June 19, 2023
+
+- installed chakra ui
+ * npm i @chakra-ui/react @emotion/react @emotion/styled framer-motion
+
+- installed react-router-dom 
+ * npm i react-router-dom
+
+ - created a file called 'routes.js' inside the src/lib' directory
+  * this file contains all routes for the project
--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
+// 2. import `RouterProvider` component
+import { RouterProvider } from 'react-router-dom';
+// 3. import `router` property
+import {router} from './lib/routes.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+  return  
+  <ChakraProvider>
+    <RouterProvider router={router} />
+  </ChakraProvider>;
 }
 
-export default App;
